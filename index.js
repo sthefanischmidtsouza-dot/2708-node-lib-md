3eac81568dfc1cc222138b2a05d2c5b3e0d267d1
sthefanischmidtsouza-dot <sthefani.schmidt.souza@escola.pr.gov.br>
2026-05-16 19:28:03 +0000

    Trabalho Finalizado 2

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.green('As séries fomentam interesses que não se restringem ao envolvimento de comunidades de fãs.'));
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+//async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.red(texto));
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+// promises com then()
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.promises
+//      .readFile(caminhoDoArquivo, encoding)
+//      .then((texto) => console.log(chalk.green(texto)))
+//      .catch(trataErro)
+//}
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8';
+//    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => { 
+//        if (erro) {
+//            trataErro(erro);
+//        }
+//     console.log(chalk.green(texto));
+//    })
+//}
+
+pegaArquivo('./arquivos/texto.md');
+
+//console.log(chalk.green('"Acredite em si mesmo e você será imparável." '));
